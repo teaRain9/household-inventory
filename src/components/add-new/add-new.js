d10386156334f56934ddf1b635339cd8ea2e3e05
teaRain9 <adel.ht@gmail.com>
2023-03-30 15:57:36 -0700

    working on add new component useRecuder

--- src/components/add-new/add-new.js
+++ src/components/add-new/add-new.js
@@ -1,75 +1,103 @@
 import classes from './add-new.module.scss'
-import {useRef, useState} from "react";
+import {useReducer, useRef, useState} from "react";
 
 export default function AddNew() {
 
-  const nameRef = useRef()
-  const [pool, setPool] = useState()
-  const [shop, setShop] = useState([])
+  const [pool, setPool] = useState('none')
   const [category, setCategory] = useState([])
+  const [formState, formDispatch] = useReducer(formReducer, 
+    { name: '',
+      nameValid: true,
+      shoppingCenter: [],
+      shoppingCenterIsValid: true,
+      category: [],
+      categoryIsValid: true,
+      pool: '',
+      formIsValid: true
+    })
 
-  const actionType = {
-    updateField: 'UPDATE_FIELD',
-    validate: 'VALIDATE_FORM'
+  const ACTION = {
+    setName: 'SET_NAME',
+    updateShop: 'UPDATE_SHOP',
+    updateCat: 'UPDATE_CATEGORY',
+    setPool: 'SET_POOL',
   }
-
 
   function formReducer(state, action) {
     switch (action.type) {
-      case 'UPDATE_FIELD':
-        return { ...state, [action.fieldName] : action.fieldValue };
-      case 'VALIDATE_FORM':
-        const errors = {};
-
-        if(!state.name) {
-          errors.name = 'Please Enter A Name!'
-        } else if(state.name.length < 3) {
-          errors. name = 'Name has be at least 3 characters long!'
+      case 'SET_NAME':
+        return { ...state, name : action.value };
+      case 'UPDATE_SHOP':
+        if(action.checked) {
+          return{...state, shoppingCenter: [...state.shoppingCenter, action.value]}
+        } else {
+          return{...state, shoppingCenter: state.shoppingCenter.filter(item => item !== action.value)}
+        }
+      case 'UPDATE_CATEGORY':
+        return {...state, category: action.value}
+      case 'UPDATE_POOL': 
+        return {...state, pool: action.value}
 
     }
   }
 
+
   function shopChangeHandler(e) {
     const {checked, value} = e.target;
-    if(checked) {
-      setShop(prevState => [...prevState, value])
-    } else {
-      setShop(prevState => prevState.filter(shop => shop !== value))
-    }
+    formDispatch({type: ACTION.updateShop, checked, value})
   }
 
 
   function addToItemsHandler(event) {
     event.preventDefault();
-    console.log(
-      {
-        name: nameRef.current.value,
-        shop: shop,
-        category: category,
-        pool: pool
-      })
+    console.log(formState)
   }
 
   return (
     <form className={classes.container} onSubmit={addToItemsHandler}>
-
       <fieldset className={classes.name_input}>
         <legend className={classes.title}>Name:</legend>
-        <input ref={nameRef} type="text" id="item-name" placeholder='Item Name:'/>
+        <input
+          type="text"
+          id="item-name"
+          name="name"
+          placeholder="Item Name:"
+          onChange={(e) =>
+            formDispatch({ type: ACTION.setName, value: e.target.value })
+          }
+        />
       </fieldset>
 
       <fieldset className={classes.selection}>
         <legend className={classes.title}>Available in:</legend>
         <div>
-          <input type="checkbox" name='shopping-center' id='costco' value='costco' onChange={shopChangeHandler}/>
+          <input
+            type="checkbox"
+            name="shoppingCenter"
+            id="costco"
+            value="costco"
+            onChange={shopChangeHandler}
+          />
           <label htmlFor="costco"> Costco</label>
         </div>
         <div>
-          <input type="checkbox" name='shopping-center' id='walmart' value='walmart' onChange={shopChangeHandler}/>
+          <input
+            type="checkbox"
+            name="shoppingCenter"
+            id="walmart"
+            value="walmart"
+            onChange={shopChangeHandler}
+          />
           <label htmlFor="walmart"> Walmart</label>
         </div>
         <div>
-          <input type="checkbox" name='shopping-center' id='meat-shop' value='meat-shop' onChange={shopChangeHandler}/>
+          <input
+            type="checkbox"
+            name="shoppingCenter"
+            id="meat-shop"
+            value="meat-shop"
+            onChange={shopChangeHandler}
+          />
           <label htmlFor="meat-shop"> Meat Shop</label>
         </div>
       </fieldset>
@@ -77,15 +105,37 @@ export default function AddNew() {
       <fieldset className={classes.selection}>
         <legend className={classes.title}>Category:</legend>
         <div>
-          <input type="radio" name='category' id='dairy' value='dairy' onChange={e => setCategory(e.target.value)}/>
+          <input
+            type="radio"
+            name="category"
+            id="dairy"
+            value="dairy"
+            onChange={(e) => setCategory(e.target.value)}
+          />
           <label htmlFor="dairy">Dairy</label>
         </div>
         <div>
-          <input type="radio" name='category' id='produce' value='produce' onChange={e => setCategory(e.target.value)}/>
+          <input
+            type="radio"
+            name="category"
+            id="produce"
+            value="produce"
+            onChange={(e) =>
+              formDispatch({ type: ACTION.updateCat, value: e.target.value })
+            }         
+            />
           <label htmlFor="produce">Produce</label>
         </div>
         <div>
-          <input type="radio" name='category' id='grain' value='grain' onChange={e => setCategory(e.target.value)}/>
+          <input
+            type="radio"
+            name="category"
+            id="grain"
+            value="grain"
+            onChange={(e) =>
+              formDispatch({ type: ACTION.updateCat, value: e.target.value })
+            }         
+          />
           <label htmlFor="grain">Grain</label>
         </div>
       </fieldset>
@@ -93,22 +143,40 @@ export default function AddNew() {
       <fieldset className={classes.selection}>
         <legend className={classes.title}>Assigned to:</legend>
         <div>
-          <input type="radio" name='pool' id='none' value='none' onChange={e => setPool(e.target.value)}/>
+          <input
+            type="radio"
+            name="pool"
+            id="none"
+            value="none"
+            checked={pool === "none"}
+            onChange={(e) => setPool(e.target.value)}
+          />
           <label htmlFor="none">None </label>
         </div>
         <div>
-          <input type="radio" name='pool' id='shopping-list' value='shopping-list' onChange={e => setPool(e.target.value)}/>
+          <input
+            type="radio"
+            name="pool"
+            id="shopping-list"
+            value="shopping-list"
+            onChange={(e) => setPool(e.target.value)}
+          />
           <label htmlFor="shopping-list">Shopping List </label>
         </div>
         <div>
-          <input type="radio" name='pool' id='inventory' value='inventory' onChange={e => setPool(e.target.value)}/>
+          <input
+            type="radio"
+            name="pool"
+            id="inventory"
+            value="inventory"
+            onChange={(e) => setPool(e.target.value)}
+          />
           <label htmlFor="inventory">Inventory </label>
         </div>
       </fieldset>
       <fieldset className={classes.submit}>
         <button className={classes.button}>Add</button>
       </fieldset>
-
     </form>
-  )
+  );
 }
